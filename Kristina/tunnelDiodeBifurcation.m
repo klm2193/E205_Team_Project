9a2aff8db5bbdfea3613acf021c8140f534bd6fd
Kristina Ming <kming@hmc.edu>
2014-11-01 21:54:22 -0700

    Working Bifurcation Plot
    Bifurcation plot without plotting imaginary fixed points

--- Kristina/tunnelDiodeBifurcation.m
+++ Kristina/tunnelDiodeBifurcation.m
@@ -20,20 +20,40 @@ x1_starVals = [];
 
 hold on
 
-for Vmax = -10:0.1:10%t = 1:length(Vin)
+for Vmax = 0:0.01:2
     clear x1
     clear x2
     syms x1 x2
     
     Vmax
     
-    eqn1 = (x2 - Vmax)/C;
+    Vd = x1;
+    Id = 17.76*Vd-103.79*Vd^2+229.62*Vd^3-226.31*Vd^4+83.72*Vd^5;
+    eqn1 = (x2 - Id)/C;
     eqn2 = (Vmax - x2*R-x1)/L;
     
     [x1_star, x2_star] = solve([eqn1, eqn2]);
     
-    plot(Vmax, x1_star, 'mo')
-    plot(Vmax, x2_star, 'co');
+    
+    if (imag(x1_star(1)) == 0)
+        plot(Vmax, x1_star(1), 'mo')
+    end
+    
+    if (imag(x1_star(2)) == 0)
+        plot(Vmax, x1_star(2), 'co')
+    end
+    
+    if (imag(x1_star(3)) == 0)
+        plot(Vmax, x1_star(3), 'bo')
+    end
+    
+    if (imag(x1_star(4)) == 0)
+        plot(Vmax, x1_star(4), 'go')
+    end
+    
+    if (imag(x1_star(5)) == 0)
+        plot(Vmax, x1_star(5), 'ro')
+    end
     
     %x1_starVals = [x1_starVals x1_star];
 end
