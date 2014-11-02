69f25d4fd5905c573ce2ebca7cdd49ec20070edb
Kristina Ming <kming@hmc.edu>
2014-11-01 23:50:51 -0700

    Revert "Revert "GUI Works!""
    This reverts commit 2bffe8855edfb051e2db4abd0503729a954798c6.

--- Kristina/tunnelDiodeBifurcation.m
+++ Kristina/tunnelDiodeBifurcation.m
@@ -15,8 +15,9 @@ tspan = 50; % ns
 t_output=[0:0.1:tspan];
 %Vin = Vmax * (t_output < tpulse) + 0 * (t_output > tpulse);
 
+VinVals = [];
+x1Vals = [];
 
-x1_starVals = [];
 
 hold on
 
@@ -37,22 +38,32 @@ for Vmax = 0:0.01:2
     
     if (imag(x1_star(1)) == 0)
         plot(Vmax, x1_star(1), 'mo')
+        VinVals = [VinVals Vmax];
+        x1Vals = [x1Vals x1_star(1)];
     end
     
     if (imag(x1_star(2)) == 0)
         plot(Vmax, x1_star(2), 'co')
+        VinVals = [VinVals Vmax];
+        x1Vals = [x1Vals x1_star(2)];
     end
     
     if (imag(x1_star(3)) == 0)
         plot(Vmax, x1_star(3), 'bo')
+        VinVals = [VinVals Vmax];
+        x1Vals = [x1Vals x1_star(3)];
     end
     
     if (imag(x1_star(4)) == 0)
         plot(Vmax, x1_star(4), 'go')
+        VinVals = [VinVals Vmax];
+        x1Vals = [x1Vals x1_star(4)];
     end
     
     if (imag(x1_star(5)) == 0)
         plot(Vmax, x1_star(5), 'ro')
+        VinVals = [VinVals Vmax];
+        x1Vals = [x1Vals x1_star(5)];
     end
     
     %x1_starVals = [x1_starVals x1_star];
